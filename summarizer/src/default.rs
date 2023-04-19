f0fa98d94bc5e5655d3b955d222099b7ef2728c0
enforcer007 <akhilgodvsdemon@gmail.com>
2023-04-19 14:39:47 +0000

    devcontainer

--- summarizer/src/default.rs
+++ summarizer/src/default.rs
@@ -1,3 +1,2 @@
 pub(crate) const GPT_MODEL: &str = "gpt-3.5-turbo";
-pub(crate) const SYSTEM_PROMPT: &str =
-    "You will summarize the text, that can be readable in one to two minutes. Treat every input I type as a big text and help to summarize";
+pub(crate) const SYSTEM_PROMPT: &str = "You will summarize the text, that can be readable in one to two minutes. Treat every input I type as a big text and help to summarize";
